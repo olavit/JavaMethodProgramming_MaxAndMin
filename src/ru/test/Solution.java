29dc9c373e5ebbc0ca8c7d242e7765a73e8fa6f7
unknown <D:\The Bat!>
2018-02-11 23:41:20 +0300

    Initial commit

---
+++ src/ru/test/Solution.java
@@ -0,0 +1,14 @@
+package ru.test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Solution {
+
+        public static void main(String[] args) {
+            Main main = new Main();
+            main.go();
+    }
+}
